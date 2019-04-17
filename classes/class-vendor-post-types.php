c55877112c94802a0f58aaddd0745c08886ecf0f
Tung Du <dinhtungdu@gmail.com>
2019-04-17 18:51:20 +0700

    Add meta box for vendor order

--- classes/class-vendor-post-types.php
+++ classes/class-vendor-post-types.php
@@ -28,21 +28,53 @@ class WCV_Post_types {
 	 * Register vendor order type
 	 */
 	public static function register_shop_order_vendor() {
+		if ( ! is_blog_installed() || post_type_exists( 'shop_order_vendor' ) ) {
+			return;
+		}
 
+		// Vendor Order
 		wc_register_order_type(
 			'shop_order_vendor',
 			apply_filters(
-				'woocommerce_register_post_type_shop_order_vendor',
+				'wcvendors_register_post_type_shop_order_vendor',
 				array(
-					'label'                            => sprintf( __( '%s Orders', 'wc-vendors' ), wcv_get_vendor_name() ),
-					'capability_type'                  => 'shop_order',
+					'labels' => array(
+						'name'               => __( 'Vendor Orders', 'wc-vendors' ),
+						'singular_name'      => __( 'Vendor Order', 'wc-vendors' ),
+						'add_new'            => _x( 'Add Vendor Order', 'custom post type setting', 'wc-vendors' ),
+						'add_new_item'       => _x( 'Add New Vendor Order', 'custom post type setting', 'wc-vendors' ),
+						'edit'               => _x( 'Edit', 'custom post type setting', 'wc-vendors' ),
+						'edit_item'          => _x( 'Edit Vendor Order', 'custom post type setting', 'wc-vendors' ),
+						'new_item'           => _x( 'New Vendor Order', 'custom post type setting', 'wc-vendors' ),
+						'view'               => _x( 'View Vendor Order', 'custom post type setting', 'wc-vendors' ),
+						'view_item'          => _x( 'View Vendor Order', 'custom post type setting', 'wc-vendors' ),
+						'search_items'       => __( 'Search Vendor Orders', 'wc-vendors' ),
+						'not_found'          => __( 'No Vendor Orders Found', 'wc-vendors' ),
+						'not_found_in_trash' => _x( 'No Vendor orders found in trash', 'custom post type setting', 'wc-vendors' ),
+						'parent'             => _x( 'Parent Orders', 'custom post type setting', 'wc-vendors' ),
+						'menu_name'          => __( 'Vendor Orders', 'wc-vendors' ),
+					),
+
+					'description'                      => __( 'This is where vendor orders are stored.', 'wc-vendors' ),
 					'public'                           => false,
+					'show_ui'                          => true,
+					'capability_type'                  => 'shop_order',
+					'map_meta_cap'                     => true,
+					'publicly_queryable'               => false,
+					'exclude_from_search'              => true,
+					'show_in_menu'                     => current_user_can( 'manage_woocommerce' ) ? 'wc-vendors' : true,
 					'hierarchical'                     => false,
-					'supports'                         => false,
+					'show_in_nav_menus'                => false,
+					'rewrite'                          => false,
+					'query_var'                        => false,
+					'supports'                         => array( 'title', 'comments', 'custom-fields' ),
+					'has_archive'                      => false,
+
+					// wc_register_order_type() params
 					'exclude_from_orders_screen'       => false,
-					'add_order_meta_boxes'             => false,
+					'add_order_meta_boxes'             => true,
 					'exclude_from_order_count'         => true,
-					'exclude_from_order_views'         => false,
+					'exclude_from_order_views'         => true,
 					'exclude_from_order_reports'       => true,
 					'exclude_from_order_sales_reports' => true,
 					'class_name'                       => 'WC_Order_Vendor',
